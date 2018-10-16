585f99645b182c8f5e6a5e38d6e69225d2c48ea2
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-16 20:16:18 +0800

    Update sectorbot.py

--- sectorbot.py
+++ sectorbot.py
@@ -4,26 +4,11 @@ import sys, traceback
 import logging
 import os
 
-logging.basicConfig(level='INFO')
 bot = commands.Bot(command_prefix=";")
-
-@bot.command()
-@commands.is_owner()
-async def die(ctx):
-    await ctx.send("Bot is shutting down.")
-    await bot.logout()
-    print("\nBot has shut down.")
 
 @bot.listen()
 async def on_ready():
-
-    print(f'\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    
-    init_status = discord.Status.online
-    init_game = discord.Game(name='Project Name Here')
-    await bot.change_presence(status=init_status, activity=init_game)
-
-    print(f'Successfully logged in and booted...!')
+    print("Ready for use!")
 
 @bot.command()
 async def hi(ctx):
